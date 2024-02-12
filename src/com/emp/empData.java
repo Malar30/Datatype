1db9c37c509312d48624f5dc3e1aae2ef9a4c77f
Malar30 <malaravs30@gmail.com>
2024-02-12 13:31:46 +0530

    sampledata

---
+++ src/com/emp/empData.java
@@ -0,0 +1,35 @@
+package com.emp;
+
+public class empData {
+	public static void main(String[] args) {
+		
+		String name = "Anand";
+		System.out.println("Employee Name is :" +name);
+		
+		char gender = 'M';
+		System.out.println("Employee Gender is :" +gender);
+		
+		byte id=127;
+		System.out.println("Employee Id is :" +id);
+		
+		short pf=8786;
+		System.out.println("Employee PF is :" +pf);
+		
+		int pincode=636408;
+		System.out.println("Employee Pincode is :" +pincode);
+		
+		long phone=89765422189L;
+				System.out.println("Employee Phone is :" +phone);
+				
+		float weight=56.78f;
+		System.out.println("Employee weight is" +weight);
+		
+		double salary=56778.91;
+		System.out.println("Employee Salary is" +salary);
+		
+		boolean nation=false;
+				System.out.println("Employee Nationality is Indian:" +nation);
+	}
+	
+
+}
